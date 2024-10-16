ab45dd10afdcf7da9b5c7f5cfa4ac36a13c3af95
seohyun <dnltjguss@gmail.com>
2024-10-16 09:25:35 +0900

    lst_com / float("INF") / my_sort

--- Python/Python_workshop/1st_241014.py
+++ Python/Python_workshop/1st_241014.py
@@ -99,8 +99,8 @@ def my_sort(lst):
 
   for i in range(n):
     for j in range(0, n-i-1):
-      if lst[i] > lst[i+1]:
-        lst[i], lst[i+1] = lst[i+1], lst[i]
+      if lst[j] > lst[j+1]:
+        lst[j], lst[j+1] = lst[j+1], lst[j]
 
   return lst
 
